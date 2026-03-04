78a35600dd472906fb62ad61c66a7d25c6babc89
miaromo <miarosemorgan05@gmail.com>
2026-03-04 22:36:31 +0000

    Add some error handling

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 # Place for backend development - python and flask/sqlite
-from flask import Flask, request
+from flask import *
 from flask_restx import Api
 
 app = Flask(__name__)
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Not for browsers!</p>"
+    abort(404)
 
-@app.route("/users",methods=["GET"])
+@app.route("/api/users",methods=["GET"])
 def showUsers():
     users = ["No users! Set up the database!"]
     return users
+
+@app.route("/api/chatrooms",methods=["GET"])
+def listChatrooms():
+    chatrooms = ["No chatrooms! Set up the database!"]
+    return chatrooms
